b2afc40472d44ed147f08da9f026c1f19d32b49f
ivor108 <61852671+ivor108@users.noreply.github.com>
2022-04-23 23:33:59 +0300

    add edges drawing and modes

--- graph3.py
+++ graph3.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'graph.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
 import math
 import random
+import warnings
 
 import numpy as np
 from PyQt5 import QtCore, QtWidgets
@@ -32,20 +26,22 @@ def get_coord(point):
 
 
 class Node(QGraphicsEllipseItem):
-    def __init__(self, x, y, w, h, text, parent, disp):
-        super().__init__(x, y, w, h)
+    def __init__(self, x, y, w, h, text, parent, disp, c):
+        super().__init__(x, y, w / c, h / c)
         self.font = QFont("Times", 20, QFont.Bold)
         self.text = QGraphicsTextItem(text, parent=self)
         self.text.setFont(self.font)
         self.text.setPos(x, y)
         self.parent = parent
         self.disp = disp
+        self.c = c
 
     def mousePressEvent(self, event):
         super(Node, self).mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
         super(Node, self).mouseReleaseEvent(event)
+        self.parent.draw_edges()
 
     def get_pos(self):
         return np.array([self.x(), self.y()])
@@ -57,7 +53,10 @@ class Edge(QGraphicsLineItem):
         self.u = node2
         pos1 = node1.get_pos()
         pos2 = node2.get_pos()
-        super().__init__(pos1[0] + 30, pos1[1] + 30, pos2[0] + 30, pos2[1] + 30)
+        super().__init__(pos1[0] + 30 / node1.c,
+                         pos1[1] + 30 / node1.c,
+                         pos2[0] + 30 / node2.c,
+                         pos2[1] + 30 / node2.c)
 
 
 class Ui_MainWindow(object):
@@ -81,9 +80,10 @@ class Ui_MainWindow(object):
         self.Shake_btn = QtWidgets.QPushButton(self.centralwidget)
         self.Shake_btn.setGeometry(QtCore.QRect(170, 540, 120, 50))
         self.Shake_btn.setObjectName("Shake_btn")
-        self.Clear_btn = QtWidgets.QPushButton(self.centralwidget)
-        self.Clear_btn.setGeometry(QtCore.QRect(290, 540, 120, 50))
-        self.Clear_btn.setObjectName("Clear_btn")
+        self.Mode_list = QtWidgets.QComboBox(self.centralwidget)
+        self.Mode_list.setGeometry(QtCore.QRect(290, 540, 120, 50))
+        self.Mode_list.addItems(["Full", "Line", "Circle", "Grid", "Tree", "Star", "Wheel"])
+        self.Mode_list.setObjectName("Mode_list")
         self.Count_spinBox = QtWidgets.QSpinBox(self.centralwidget)
         self.Count_spinBox.setGeometry(QtCore.QRect(0, 540, 50, 50))
         self.Count_spinBox.setMinimum(2)
@@ -104,7 +104,6 @@ class Ui_MainWindow(object):
         self.Random_btn.setGeometry(QtCore.QRect(700, 540, 90, 50))
         self.Random_btn.setObjectName("Random_btn")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.vel = math.sqrt(self.width*self.height)*0.1
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -116,24 +115,24 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.Draw_btn.setText(_translate("MainWindow", "Draw"))
         self.Shake_btn.setText(_translate("MainWindow", "Move"))
-        self.Clear_btn.setText(_translate("MainWindow", "One step"))
         self.Import_btn.setText(_translate("MainWindow", "Import JSON"))
         self.Random_btn.setText(_translate("MainWindow", "Random"))
 
     def add_functions(self):
         self.Draw_btn.clicked.connect(lambda: self.draw_random_nodes())
         self.Shake_btn.clicked.connect(lambda: self.move_nodes(self.Speed_list.currentText()))
-        self.Clear_btn.clicked.connect(lambda: self.one_step())
         self.Count_spinBox.valueChanged.connect(lambda: self.set_matrix())
         self.Import_btn.clicked.connect(lambda: self.import_json('data/tree3_simple.json'))
         self.Random_btn.clicked.connect(lambda: self.random_graph())
+        self.Mode_list.currentTextChanged.connect(lambda: self.set_mode())
 
     def check_symmetric(self):
         matrix = str_to_matrix(self.Matrix_TextEdit.toPlainText())
         return np.allclose(matrix, matrix.T)
 
     def set_matrix(self):
-        self.Matrix_TextEdit.setText(str(np.ones((self.Count_spinBox.value(), self.Count_spinBox.value())).astype(int)))
+        # self.Matrix_TextEdit.setText(str(np.ones((self.Count_spinBox.value(), self.Count_spinBox.value())).astype(int)))
+        self.Matrix_TextEdit.setText(self.get_matrix_mode(self.Mode_list.currentText()))
 
     def get_nodes(self):
         nodes = [item for item in self.scene.items() if isinstance(item, Node)]
@@ -169,15 +168,15 @@ class Ui_MainWindow(object):
                     # print(nodes[i], nodes[j])
 
     def draw_node(self, x, y, text):
-        node = Node(0, 0, 50, 50, text, parent=self, disp=np.array([0, 0]))
+        node = Node(0, 0, 50, 50, text,
+                    parent=self,
+                    disp=np.array([0, 0]),
+                    c=1)
         node.setPos(x, y)
+        node.setZValue(1)
         node.setBrush(QBrush(Qt.red))
         node.setFlag(QGraphicsItem.ItemIsMovable)
         self.scene.addItem(node)
-
-    def redraw_node(self, v, x, y):
-        v.setPos(x, y)
-
 
     def draw_nodes(self, coord_array):
         self.scene.clear()
@@ -207,7 +206,9 @@ class Ui_MainWindow(object):
         family_tree = FamilyTree(open_json(file_name))
         df = family_tree.get_links_pair_df()
         G = nx.from_pandas_edgelist(df, 'source_id', 'target_id')
-        self.Matrix_TextEdit.setText(str(nx.adjacency_matrix(G).toarray()))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.Matrix_TextEdit.setText(str(nx.adjacency_matrix(G).toarray()))
         # self.Count_spinBox.setValue(len(G.nodes()))
 
     def random_graph(self):
@@ -219,14 +220,14 @@ class Ui_MainWindow(object):
         self.Matrix_TextEdit.setText(str(result))
 
     def f_a(self, x):
-        k = np.sqrt(self.width * self.height / len(self.get_nodes()))
+        k = np.sqrt(self.width * self.height / len(self.get_nodes())) / 5
         return x ** 2 / k
 
     def f_r(self, x):
-        k = np.sqrt(self.width * self.height / len(self.get_nodes()))
+        k = np.sqrt(self.width * self.height / len(self.get_nodes())) / 5
         return k ** 2 / x
 
-    def one_step(self):
+    def one_step(self, vel):
         V = self.get_nodes()
         E = self.get_edges()
         # сила отталкивания
@@ -243,25 +244,82 @@ class Ui_MainWindow(object):
             e.v.disp = e.v.disp - (delta / np.linalg.norm(delta)) * self.f_a(np.linalg.norm(delta))
             e.u.disp = e.u.disp + (delta / np.linalg.norm(delta)) * self.f_a(np.linalg.norm(delta))
 
-
         # перемещаем узлы
         for v in V:
             # вычисляем новые координаты
-            new_pos = v.get_pos() + (v.disp / np.linalg.norm(v.disp)) * self.vel
+            new_pos = v.get_pos() + (v.disp / np.linalg.norm(v.disp)) * vel
             # огранение выхода узла за рамки
             new_pos[0] = min((self.width - 50), max(0, new_pos[0]))
             new_pos[1] = min((self.height - 50), max(0, new_pos[1]))
             # перемещаем
             v.setPos(new_pos[0], new_pos[1])
-        self.vel *= 0.97
 
     def move_nodes(self, speed):
-        while self.vel > 0.1:
-            QtTest.QTest.qWait(1)
-            self.one_step()
+        vel = math.sqrt(self.width * self.height) * 0.1
+        while vel > 0.1:
+            if speed.isdigit():
+                # QtTest.QTest.qWait(int(100 ** (1 / int(speed))))
+                QtTest.QTest.qWait(int(speed))
+            # QtTest.QTest.qWait(1)
+            self.one_step(vel)
             self.draw_edges()
-            print('test')
+            vel *= 0.97
+            print(vel)
 
+    def set_mode(self):
+        self.Matrix_TextEdit.setText(self.get_matrix_mode(self.Mode_list.currentText()))
+
+    def get_matrix_mode(self, mode):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            edges = []
+            n = self.Count_spinBox.value()
+
+            if mode == 'Line':
+                for i in range(n - 1):
+                    edges.append((i, i + 1))
+
+            if mode == 'Circle':
+                edges = [(n - 1, 0)]
+                for i in range(n - 1):
+                    edges.append((i, i + 1))
+
+            if mode == 'Grid':
+                h = 3
+                w = 3
+                for i in range(h):
+                    for j in range(w - 1):
+                        u = i * w + j
+                        v = u + 1
+                        edges.append((u, v))
+                for j in range(w):
+                    for i in range(h - 1):
+                        u = i * w + j
+                        v = u + w
+                        edges.append((u, v))
+
+            if mode == 'Tree':
+                for i in range(1, n):
+                    j = (i - 1) // 2
+                    edges.append((j, i))
+
+            if mode == 'Star':
+                for i in range(1, n):
+                    edges.append((0, i))
+
+            if mode == 'Wheel':
+                for i in range(1, n):
+                    edges.append((0, i))
+                    j = i + 1 if i < n - 1 else 1
+                    edges.append((i, j))
+
+            if mode == 'Full':
+                for i in range(n - 1):
+                    for j in range(i + 1, n):
+                        edges.append((i, j))
+
+            G = nx.Graph(edges)
+            return str(nx.adjacency_matrix(G).toarray())
 
 
 
